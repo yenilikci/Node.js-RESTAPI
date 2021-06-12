b56b7cedf8516438805c01a7de8480244915d98d
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-12 14:16:40 +0300

    Basit bir cevap gönderme, writeHead kullanımı, kullanıcıların gönderilmesi

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 //http paketleri
 const http = require('http')
+//kullanıcı verileri
+const kullanicilar = require('./data/kullaniciDB.json')
 
 //server oluşturma
 const server = http.createServer((req,res) => {
-    console.log(req.url)
-    console.log(req.method);
+    //console.log(req.url)
+    //console.log(req.method);
+    //res.statusCode = 200
+    //res.setHeader('Content-Type','text/html')
+    //res.write('<h1>Merhaba Node.js</h1>')
+    //res.end()
+    res.writeHead(200,{'Content-Type':'application/json'})
+    res.end(JSON.stringify(kullanicilar))
 })
 
 //port numarası ayarlaması
